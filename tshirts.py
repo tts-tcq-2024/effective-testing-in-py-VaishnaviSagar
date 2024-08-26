43dd7b814f3c99255f85a1b0ac6ef485cb36e43e
VaishnaviSagar <67277305+VaishnaviSagar@users.noreply.github.com>
2024-08-26 09:25:57 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -8,13 +8,13 @@ def size(cms):
         return 'L'
 
 
-      assert(size(37) == 'S')
-      assert(size(38) == 'L')
-      assert(size(40) == 'M')
-      assert(size(42) == 'L')
-      assert(size(43) == 'L')
-      assert(size(37.9) == 'S')
-      assert(size(38.1) == 'M')
-      assert(size(41.9) == 'M')
-      assert(size(42.1) == 'L')
+assert(size(37) == 'S')
+assert(size(38) == 'L')
+assert(size(40) == 'M')
+assert(size(42) == 'L')
+assert(size(43) == 'L')
+assert(size(37.9) == 'S')
+assert(size(38.1) == 'M')
+assert(size(41.9) == 'M')
+assert(size(42.1) == 'L')
 print("All is well (maybe!)\n")
